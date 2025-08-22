783aee3281c6673bea385391750e7f9029fc0af8
Vaibhav Tandon <tvaibhav1306@gmail.com>
2025-08-22 23:58:23 +0530

    Time: 0 ms (100%), Space: 41 MB (77.02%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,16 +1,13 @@
 class Solution {
-    public int jumps(int[] nums,int i,int[] dp){
-        if(i>=nums.length)return 0;
-        if(dp[i]!=-1)return dp[i];
-        int rob=nums[i]+jumps(nums,i+2,dp);
-        int skip=jumps(nums,i+1,dp);
-        dp[i]=Math.max(skip,rob);
-        return dp[i];
-    }
     public int rob(int[] nums) {
-        int[] dp=new int[nums.length];
-        Arrays.fill(dp,-1);
-        int f=jumps(nums,0,dp);
-        return f;
+        int n=nums.length;
+        if(n==1)return nums[0];
+        int[] dp=new int[n];
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[n-1];
     }
 }
