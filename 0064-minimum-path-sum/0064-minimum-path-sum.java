eaa6e81a40969676da59aa01bbb97b04db7d0207
Vaibhav Tandon <tvaibhav1306@gmail.com>
2025-08-30 02:01:16 +0530

    Time: 5 ms (27.16%), Space: 47.9 MB (31.32%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -2,7 +2,6 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        if(m==1 && n==1)return grid[0][0];
         int[][] dp = new int[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
