9094340a3e730886c49638d7f2a5a9e94fb01782
Vaibhav Tandon <tvaibhav1306@gmail.com>
2026-01-12 16:20:12 +0530

    Time: 2 ms (12.64%), Space: 47 MB (29.23%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,38 +1,45 @@
-class Pair{
+class pair{
     int r;
     int c;
-    Pair(int r,int c){
+    public pair(int r,int c){
         this.r=r;
         this.c=c;
     }
 }
 class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int m=image.length,n=image[0].length;
-        int[][] visited=new int[m][n];
-        visited[sr][sc]=1;
-        if(image[sr][sc]==color)return image;
-        Queue<Pair> q=new LinkedList<>();
-        q.offer(new Pair(sr,sc));
-        int startcol=image[sr][sc];
-        image[sr][sc]=color;
-        int[] drow={-1,0,+1,0};
-        int[] dcol={0,+1,0,-1};
+    public void bfs(int[][] copy,int sr,int sc,int[][] visited,int color,int startcolor){
+        int m=copy.length;
+        int n=copy[0].length;
+        Queue<pair> q=new LinkedList<>();
+        q.add(new pair(sr,sc));
+        int[] drow={-1,0,1,0};
+        int[] dcol={0,1,0,-1};
         while(!q.isEmpty()){
-            int r=q.peek().r;
-            int c=q.peek().c;
-            q.poll();
-            for(int i=0;i<4;i++){
-                int nrow=r+drow[i];
-                int ncol=c+dcol[i];
-                if(nrow>=0&& nrow<=m-1 && ncol>=0&& ncol<=n-1
-                && visited[nrow][ncol]==0 && image[nrow][ncol]==startcol){
+             int row=q.peek().r;
+             int col=q.peek().c;
+             q.remove();
+             visited[row][col]=1;
+             for(int i=0;i<4;i++){
+                int nrow=row+drow[i];
+                int ncol=col+dcol[i];
+                if(nrow>=0&&ncol>=0&&nrow<m&&ncol<n&&visited[nrow][ncol]==0&&copy[nrow][ncol]==startcolor){
+                    q.add(new pair(nrow,ncol));
                     visited[nrow][ncol]=1;
-                    image[nrow][ncol]=color;
-                    q.offer(new Pair(nrow,ncol));
+                    copy[nrow][ncol]=color;
                 }
-            }
+             }
         }
-        return image;
+    }
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int m=image.length;
+        int n=image[0].length;
+        int[][] copy=new int[m][n];
+        int[][] visited=new int[m][n];
+        copy=image;
+        int startcolor=copy[sr][sc];
+        copy[sr][sc]=color;
+        visited[sr][sc]=1;
+        bfs(copy,sr,sc,visited,color,startcolor);
+        return copy;
     }
 }
