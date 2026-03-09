bfef8996b6986a4d6797e9204a711225c1d46e7f
Vaibhav Tandon <tvaibhav1306@gmail.com>
2026-03-10 01:30:45 +0530

    Time: 0 ms (100%), Space: 42.1 MB (52.25%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,12 @@
 class Solution {
-    public int recur(int i,int[] dp){
-       if(i<0)return 0;
-       else{
-            if(i==0)return 1;
-            else if(dp[i-1]!=-1)return dp[i-1];
-       }
-       return dp[i-1]=(recur(i-1,dp)+recur(i-2,dp));
-    }
+
     public int climbStairs(int n) {
-        int[] dp=new int[n];
-        Arrays.fill(dp,-1);
-        int ans=recur(n,dp);
-        return dp[n-1];
+        int[] dp=new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
