0516e17346ccaa2cf6cb71bd8fb00cb57a80c28d
Vaibhav Tandon <tvaibhav1306@gmail.com>
2026-03-13 01:18:14 +0530

    Time: 1 ms (2.6%), Space: 42.9 MB (52.14%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,22 +1,38 @@
 class Solution {
-    public int jump(int[] nums,int i,int[] dp){
-        int n=nums.length;
-        if(i>=n-2)return nums[i];
-        if(dp[i]!=-1)return dp[i];
-        int sum=nums[i],max=Integer.MIN_VALUE;
-        for(int j=i+2;j<n;j++){
-            int jump=jump(nums,j,dp);
-            max=Math.max(max,jump);
+    public int jump(int[] nums, int i, int[] dp) {
+        int n = nums.length;
+        if (i >= n - 2)
+            return nums[i];
+        if (dp[i] != -1)
+            return dp[i];
+        int sum = nums[i], max = Integer.MIN_VALUE;
+        for (int j = i + 2; j < n; j++) {
+            int jump = jump(nums, j, dp);
+            max = Math.max(max, jump);
         }
-        sum+=max;
-        dp[i]=sum;
+        sum += max;
+        dp[i] = sum;
         return dp[i];
     }
+
     public int rob(int[] nums) {
-       int n=nums.length;
-       int[] dp=new int[n];
-       Arrays.fill(dp,-1);
-       if(n==1)return nums[0];
-       return Math.max(jump(nums,0,dp),jump(nums,1,dp));
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        dp[0] = nums[0];
+        if (n == 1)
+            return nums[0];
+        dp[1] = nums[1];
+        for (int i = 2; i < n; i++) {
+            int max = Integer.MIN_VALUE;
+
+            for (int j = 0; j <= i - 2; j++) {
+                int jump = nums[i];
+                int prev = dp[j];
+                max = Math.max(max, jump + prev);
+            }
+            dp[i] = max;
+        }
+        return Math.max(dp[n - 2], dp[n - 1]);
     }
 }
